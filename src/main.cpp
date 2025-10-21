6ca8e8190585f3b14c3920d055bba90c290f6bab
mtmjnb <fakemtmjnb@gmail.com>
2025-10-21 15:53:25 +0100

    Add temporary main.cpp file with basic SFML code

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+// The entire contents of this file are temporary
+#include <SFML/Graphics.hpp>
+
+int main(int argc, char** argv) {
+    sf::RenderWindow window = sf::RenderWindow(sf::VideoMode({ 1920u, 1080u }), "CMake SFML Project");
+    window.setFramerateLimit(144);
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+    return 0;
+}
