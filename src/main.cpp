22f19d867ca0c71ceb6cdb71c9745275118a7425
mtmjnb <fakemtmjnb@gmail.com>
2025-11-11 15:43:26 +0000

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,13 @@
-// The entire contents of this file are temporary
-#include <SFML/Graphics.hpp>
-#include "engine/tile_engine.h"
-#include "engine/renderer.h"
+#include "engine/game_system.h"
+#include "game_parameters.h"
+#include "scenes.h"
 
-int main(int argc, char** argv) {
-    sf::RenderWindow window = sf::RenderWindow(sf::VideoMode({ 1920u, 1080u }), "CMake SFML Project");
-    window.setFramerateLimit(144);
-    Renderer::initialise(window);
-    LevelSystem::load_level("res/levels/temp_level.txt", 150.0f);
-    sf::CircleShape shape(75.0f);
-    shape.setFillColor(sf::Color::Green);
-    shape.setPosition(LevelSystem::get_start_position());
-
-    while (window.isOpen()) {
-        while (const std::optional event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) {
-                window.close();
-                Renderer::shutdown();
-            }
-        }
-
-        window.clear();
-        LevelSystem::render();
-        Renderer::render();
-        window.draw(shape);
-        window.display();
-    }
+int main() {
+    Scenes::menu = std::shared_ptr<MenuScene>(new MenuScene());
+    Scenes::menu->load();
+    Scenes::game = std::make_shared<GameScene>();
+    Scenes::game->load();
+    GameSystem::set_active_scene(Scenes::menu);
+    GameSystem::start(Parameters::game_width, Parameters::game_height, "Foodies", Parameters::time_step);
     return 0;
 }
