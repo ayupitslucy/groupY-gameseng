9d45d06bd70db766df4df46927ba4e63d2fb5ad4
mtmjnb <fakemtmjnb@gmail.com>
2025-11-11 15:41:25 +0000

    Add game parameters

---
+++ src/game_parameters.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct Parameters {
+    static constexpr int game_width = 1280;
+    static constexpr int game_height = 720;
+    static constexpr float time_step = 0.017f; // 60 fps
+    static constexpr const char* level_1 = "res/levels/temp_level.txt";
+};
